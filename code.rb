e9f9849d32394313b06adcc763657f94961cd265
Morgan Webb <mnbwebb2233@gmail.com>
2020-03-24 09:31:27 +1100

    Initial commit

---
+++ code.rb
@@ -0,0 +1,42 @@
+names = [
+    "Aaron Lewis",
+    "Adam Payne",
+    "Andrew Tran",
+    "Anh Nguyen",
+    "Ashley Bridges",
+    "Christopher Morris",
+    "Vicky Pham",
+    "Esther Csoke",
+    "Evelyn Paplauskas",
+    "Gary Ho",
+    "Gregory Martin",
+    "Harrison Powell",
+    "Harry Buisman",
+    "Hoang Hai Ho",
+    "Hugh Greethead",
+    "Jacob Colyvan",
+    "Jeremy Wu",
+    "Linda Ojinnaka",
+    "Ling Yen Lee",
+    "Martin Kennedy",
+    "Martin Valdivia",
+    "Matthew Molloy",
+    "Morgan Webb",
+    "Morgan Batterham",
+    "Nikki Waelkens",
+    "Oliver Mason",
+    "Patricia Pavia",
+    "Rederyck Vazquez",
+    "Rose Hernandez",
+    "Ross Baker",
+    "Ryan Cook",
+    "Vincent Lieu",
+    "Gini Thomas",
+    "Wendy Doan",
+    "Wilkins Cheng"]
+
+5.times {
+    system("clear")
+    puts("#{names.sample}")
+    sleep(0.5)
+}
